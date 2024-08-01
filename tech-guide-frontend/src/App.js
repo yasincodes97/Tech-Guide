966f98279d70c3021148cd769edc1fc298147aea
Yasin Kapisiz <yasink.97@outlook.de>
2024-08-02 01:03:36 +0200

    test branch

--- tech-guide-frontend/src/App.js
+++ tech-guide-frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; //standard import
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import PythonBasics from './components/PythonBasics';
+import WebDevBasics from './components/WebDevelopmentBasics';
+import AdvancedConc from './components/AdvancedConcepts';
+import Exercises from './components/Exercises';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
+function App() { //entry point for React-Applications
+  return (      // JS is executed directly in the browser
+                //JSX extension to JS, JS-XML is transformed to 
+                //JS with babel-Transpiler-Tool, React-Component
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+
+
+      </Switch>
+
     </div>
   );
 }
